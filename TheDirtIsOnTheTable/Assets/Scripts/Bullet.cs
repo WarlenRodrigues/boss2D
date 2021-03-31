2fd99b61af70addc6eec0985625923f1ff09d9e4
Gabriel Noal <gabriel.oliva@hotmail.com>
2021-03-30 21:24:36 -0300

    create Example Scene and add sprites

---
+++ TheDirtIsOnTheTable/Assets/Scripts/Bullet.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public Rigidbody2D slingShot;
+    public GameObject nextBullet;
+
+    public float releaseTime = 0.15f;
+    public float maxDragDistance = 2.0f;
+
+
+    private bool isPressed = false;
+
+    private void Update()
+    {
+        if (isPressed)
+        {
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 slingShotPos = slingShot.position;
+            if (Vector3.Distance(mousePos, slingShotPos) > maxDragDistance)
+                rb.position = slingShotPos + (mousePos - slingShotPos).normalized * maxDragDistance;
+            else
+                rb.position = mousePos;
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        isPressed = true;
+        rb.isKinematic = true;
+    }
+
+    private void OnMouseUp()
+    {
+        isPressed = false;
+        rb.isKinematic = false;
+
+        StartCoroutine(Release());
+    }
+
+    IEnumerator Release()
+    {
+        yield return new WaitForSeconds(releaseTime);
+
+        GetComponent<SpringJoint2D>().enabled = false;
+        this.enabled = false;
+
+        yield return new WaitForSeconds(2.0f);
+
+        if (nextBullet != null)
+        {
+            nextBullet.SetActive(true);
+        }
+        else
+        {
+            Target.TargetsOverTheTable = 0;
+            Debug.Log("LOST");
+        }
+    }
+}
---
+++ TheDirtIsOnTheTable/Assets/Scripts/Target.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+
+    public static int TargetsOverTheTable = 0;
+    void Start()
+    {
+        TargetsOverTheTable++;
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("floor"))
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        TargetsOverTheTable--;
+
+        if (TargetsOverTheTable <= 0)
+            Debug.Log("WON");
+
+        Destroy(gameObject);
+
+    }
+}
