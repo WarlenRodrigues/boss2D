9ad2badd1c64498b5a24af4cf5efd489d4eb9016
WarlenRodrigues <warlen.cesarrodrigues@gmail.com>
2021-04-05 22:45:54 -0300

    Added screen managemente to select levels

---
+++ TheDirtIsOnTheTable/Assets/Scripts/MainMenuSelectScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuSelectScript : MonoBehaviour
+{
+
+    public void selectScene() {
+        switch (this.gameObject.name)
+        {
+            case "Level1Button":
+                SceneManager.LoadScene("ExampleScene");
+                break;
+            case "Level2Button":
+                SceneManager.LoadScene("Transition2");
+                break;
+            case "Level3Button":
+                SceneManager.LoadScene("Transition3");
+                break;
+
+        }
+    }
+
+}
