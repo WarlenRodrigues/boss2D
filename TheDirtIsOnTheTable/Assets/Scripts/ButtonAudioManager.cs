8bca223e49b81bf8bbdf0167bf9587ce35a343df
WarlenRodrigues <warlen.cesarrodrigues@gmail.com>
2021-04-23 17:25:48 -0300

    [FEAT] Added bg music and click SFX

---
+++ TheDirtIsOnTheTable/Assets/Scripts/ButtonAudioManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class ButtonAudioManager : MonoBehaviour
+{
+    [SerializeField]
+    private AudioSource sfxSource = default;
+    [SerializeField]
+    private static ButtonAudioManager _instance;
+
+    public static void PlaySFX(AudioClip audioClip)
+    {
+        _instance.sfxSource.PlayOneShot(audioClip);
+    }
+
+}
