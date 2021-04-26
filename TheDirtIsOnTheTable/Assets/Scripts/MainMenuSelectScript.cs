583da26a3be18584b392835007f73f7298f83715
Gabriel Noal <gabriel.oliva@hotmail.com>
2021-04-26 19:03:23 -0300

    add new levels

--- TheDirtIsOnTheTable/Assets/Scripts/MainMenuSelectScript.cs
+++ TheDirtIsOnTheTable/Assets/Scripts/MainMenuSelectScript.cs
@@ -8,7 +8,6 @@ public class MainMenuSelectScript : MonoBehaviour
     GameManager gm;
     public AudioClip clickSound;
 
-
     void Start()
     {
         gm = GameManager.GetInstance();
@@ -17,6 +16,7 @@ public class MainMenuSelectScript : MonoBehaviour
     public void selectScene()
     {
         IngameAudioManager.PlaySFX(clickSound);
+        
 
         switch (this.gameObject.name)
         {
@@ -32,7 +32,30 @@ public class MainMenuSelectScript : MonoBehaviour
                 gm.setCurrentLevel(3);
                 SceneManager.LoadScene("Level_3");
                 break;
-
+            case "Level4":
+                gm.setCurrentLevel(4);
+                SceneManager.LoadScene("Level_4");
+                break;
+            case "Level5":
+                gm.setCurrentLevel(5);
+                SceneManager.LoadScene("Level_5");
+                break;
+            case "Level6":
+                gm.setCurrentLevel(6);
+                SceneManager.LoadScene("Level_6");
+                break;
+            case "Level7":
+                gm.setCurrentLevel(7);
+                SceneManager.LoadScene("Level_7");
+                break;
+            case "Level8":
+                gm.setCurrentLevel(8);
+                SceneManager.LoadScene("Level_8");
+                break;
+            case "Level9":
+                gm.setCurrentLevel(9);
+                SceneManager.LoadScene("Level_9");
+                break;
         }
     }
 
